a23b5b5d78b5a8ab956de0ffe4068507fbdbda88
José Padilla <jpadilla@webapplicate.com>
2019-07-14 07:33:36 -0400

    Add custom exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS();
+module.exports = withCSS({
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    let paths = {
+      '/': { page: '/' }
+    };
+
+    for (let i = 0; i < 889; i++) {
+      let pageNumber = `${i + 1}`;
+      paths[`/page/${pageNumber}`] = { page: '/page/[pageNumber]', query: { pageNumber } };
+    }
+
+    return paths;
+  }
+});
