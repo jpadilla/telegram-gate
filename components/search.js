e89cce925863a871fe62aa277167afbc3e78043a
José Padilla <jpadilla@webapplicate.com>
2019-07-14 09:19:18 -0400

    Fix search button submit

--- components/search.js
+++ components/search.js
@@ -24,7 +24,7 @@ function Search(props) {
         />
         <button
           className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
-          type="button"
+          type="submit"
         >
           Buscar
         </button>
