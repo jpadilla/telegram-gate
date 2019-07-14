55a9f64228e6f7a3dee989c1b378ff8b5dbcdab0
José Padilla <jpadilla@webapplicate.com>
2019-07-14 09:23:09 -0400

    Tweak search field

--- components/search.js
+++ components/search.js
@@ -13,10 +13,10 @@ function Search(props) {
 
   return (
     <form method="GET" onSubmit={handleSubmit}>
-      <div className="flex items-center border-b border-b-2 border-blue-500 py-2">
+      <div className="flex items-center pt-4">
         <input
           name="q"
-          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full mr-3 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
           type="text"
           placeholder="Busca por términos en el chat de Telegram, ejemplo: twitter"
           defaultValue={props.query}
