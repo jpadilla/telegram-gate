704e993325c82e602ba87d5153a7ba33ba639fb6
Froilan Irizarry <froi@github.com>
2019-08-03 15:13:05 -0400

    Adding Lunr.js (#9)
    * Added create-index npm command and script
    
    * Fixed elOtroIndex creation.
    
    * Initial commit of lunr indexes
    
    * Added lunr search and lunr index import.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Head from 'next/head';
 import Header from '../components/header';
 import Page from '../components/page';
 import data from '../static/chat.json';
+import telegramGateIndex from '../static/telegram-gate-index.json';
 import unidecode from 'unidecode';
+import lunr from 'lunr'
 
 function Home(props) {
   return (
@@ -39,13 +41,22 @@ function Home(props) {
 
 Home.getInitialProps = async function(context) {
   let query = context.query.q;
-  if (query) {
-    let normalizedQuery = unidecode(query.toLowerCase());
-    let results = data.filter((result) =>
-      result.text.includes(normalizedQuery)
-    );
 
-    return { results, query };
+  if (query) {
+    const normalizedQuery = unidecode(query.toLowerCase());
+
+    // Load preconstructed index
+    const idx = lunr.Index.load(telegramGateIndex);
+
+    const results = idx.search(normalizedQuery);
+
+    // Convert all references to numbers. These are used to filter the documents in data.
+    const refs = results.map(result => parseInt(result.ref));
+
+    return { 
+      results: data.filter(result => refs.includes(result.page)), 
+      query
+    };
   }
 
   return { results: data, query: '' };
---
+++ scripts/creater-lunr-index.js
@@ -0,0 +1,33 @@
+const lunr = require('lunr');
+const telegramGate = require('../static/chat.json');
+const elOtro = require('../static/chat-otro.json');
+const fs = require('fs');
+const path = require('path');
+
+const staticPath = path.join(path.dirname(__dirname), 'static');
+
+const telegramGateIndex = lunr(function() {
+    this.ref('page');
+    this.field('text');
+
+    telegramGate.forEach(message => this.add(message), this);
+});
+
+const elOtroIndex = lunr(function() {
+    this.ref('page');
+    this.field('text');
+
+    elOtro.forEach(message => this.add(message), this);
+});
+
+fs.writeFile(path.join(staticPath, 'telegram-gate-index.json'), JSON.stringify(telegramGateIndex), error => {
+    if(error) {
+        console.error('ERROR writing telegramGateIndex index to file.', error);
+    }
+});
+
+fs.writeFile(path.join(staticPath, 'el-otro-index.json'), JSON.stringify(elOtroIndex), error => {
+    if(error) {
+        console.error('ERROR writing elOtro index to file.', error);
+    }
+});
